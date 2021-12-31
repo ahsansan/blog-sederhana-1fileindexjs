60ef84697b187afdc2da5e0279b9f01908a12c48
ahsansan <93358769+ahsansan@users.noreply.github.com>
2021-12-31 08:31:31 +0700

    db

--- connection/db.js
+++ connection/db.js
@@ -1,11 +1,13 @@
 const { Pool } = require('pg')
 
 const dbPool = new Pool({
-	host:'ec2-34-236-87-247.compute-1.amazonaws.com' ,
-	database: 'dhr77gq8mle3s',
-	port: 5432,
-	user: 'caiydwekzxtwtk',
-	password: 'ce3349aee2d73b2d3a470211fa7329527b6e0515b3ab61c053bd740aff5349f8'
+	// host:'ec2-34-236-87-247.compute-1.amazonaws.com' ,
+	// database: 'dhr77gq8mle3s',
+	// port: 5432,
+	// user: 'caiydwekzxtwtk',
+	// password: 'ce3349aee2d73b2d3a470211fa7329527b6e0515b3ab61c053bd740aff5349f8'
+	connectionString: 'postgres://caiydwekzxtwtk:ce3349aee2d73b2d3a470211fa7329527b6e0515b3ab61c053bd740aff5349f8@ec2-34-236-87-247.compute-1.amazonaws.com:5432/dhr77gq8mle3s',
+	ssl: { rejectUnauthorized: false}
 })
 
 module.exports = dbPool
